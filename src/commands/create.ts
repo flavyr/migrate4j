0cc50879e404f31629ce818d6b502e2fc70534a1
Spencer Miller <dogpantz@gmail.com>
2020-09-12 21:01:21 -0600

    add support for creating JS migrations

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -17,7 +17,7 @@ function getTimestamp() {
 }
 
 // prettier-ignore
-const getFileTemplate = (name: string) =>
+const getTsFileTemplate = (name: string) =>
 `import { Migration } from 'migrate4j';
 
 export default class ${name} extends Migration {
@@ -30,6 +30,22 @@ export default class ${name} extends Migration {
   }
 }`;
 
+// prettier-ignore
+const getJsFileTemplate = (name: string) =>
+`const { Migration } = require('migrate4j');
+
+class ${name} extends Migration {
+  async up(tx) {
+
+  }
+
+  async down(tx) {
+
+  }
+}
+
+module.exports.default = test;`;
+
 export default class Create extends Command {
   static description = 'Create a new migration file prefixed with the current datetime.';
 
@@ -38,6 +54,10 @@ export default class Create extends Command {
     prefix: flags.string({
       char: 'p',
       description: 'Prefix to use instead of current datetime',
+    }),
+    js: flags.boolean({
+      char: 'j',
+      description: 'Create a JS template. Optionally configurable by M4J_JS env var.',
     }),
   };
 
@@ -52,10 +72,16 @@ export default class Create extends Command {
   async run() {
     const { args, flags } = this.parse(Create);
     const prefix = flags.prefix ?? getTimestamp();
+    const useJs = flags.js || process.env.M4J_JS;
     if (args.name) {
       const migrator = new Migrator();
-      const fileName = `${prefix}_${args.name}.ts`;
+
+      const extension = useJs ? 'js' : 'ts';
+      const getFileTemplate = useJs ? getJsFileTemplate : getTsFileTemplate;
+
+      const fileName = `${prefix}_${args.name}.${extension}`;
       const filePath = path.resolve(migrator.migrationsDir, fileName);
+
       this.log(`Creating migration at ${migrator.migrationsDir}/${fileName}`);
       writeFileSync(filePath, getFileTemplate(args.name));
     }
